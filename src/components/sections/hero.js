cdb6e8879a1e1185df5357a73f384b2544224315
PaParaZz1 <niuyazhe314@outlook.com>
2025-10-30 01:07:53 +0800

    fix: add local test pass version

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,27 +61,25 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Yazhe Niu.</h2>;
-  const three = <h3 className="big-heading">I build things for AI-native games and their creation platform.</h3>;
+  const three = (
+    <h3 className="big-heading">I build things for AI-native games and their creation platform.</h3>
+  );
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I'm an researcher who specializes in deep reinforcement learning and game AI, aiming to
+        create proactive multi-modal interactions. Currently, I’m focused on building independent
+        games with{' '}
+        <a href="https://en.wikipedia.org/wiki/Roguelike" target="_blank" rel="noreferrer">
+          Roguelike characteristics
         </a>
         .
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="https://github.com/opendilab" target="_blank" rel="noreferrer">
+      Check out our projects on GitHub!
     </a>
   );
 
