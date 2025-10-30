fca6bba02b64cc2ca859ead4b60ddc6dcfab58d0
PaParaZz1 <niuyazhe314@outlook.com>
2025-10-30 13:43:32 +0800

    feature: finalize contact info

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,10 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I'm launching Gee-Yee (极诣), an AI x Game startup dedicated to pioneering the next frontier
+        of human-AI symbiosis. We are creating games with novel, proactive multi-modal interactions
+        and building the open platform to power this new creative paradigm We warmly invite those
+        who share our vision to contribute ideas worth spreading.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
